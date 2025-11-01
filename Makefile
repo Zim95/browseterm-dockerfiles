e2defcc25f5b1c5b889737b49589fdfbb4272e01
namah <namah@lyric.tech>
2025-11-01 21:32:54 +0530

    Added REPO_NAME in test_deployments and Makefile

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ build_status_sidecar:
 	./status_sidecar/infra/build.sh $(USER_NAME) $(REPO_NAME)
 
 test_deployment_setup:
-	./test_deployment/test_deployment_setup.sh $(NAMESPACE) $(CONTAINER_ID) $(DB_HOST) $(DB_PORT) $(DB_USERNAME) $(DB_PASSWORD) $(DB_DATABASE)
+	./test_deployment/test_deployment_setup.sh $(NAMESPACE) $(REPO_NAME) $(CONTAINER_ID) $(DB_HOST) $(DB_PORT) $(DB_USERNAME) $(DB_PASSWORD) $(DB_DATABASE)
 
 test_deployment_teardown:
 	./test_deployment/test_deployment_teardown.sh $(NAMESPACE)
--- test_deployment/test_deployment_setup.sh
+++ test_deployment/test_deployment_setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 <namespace> <container-id> <db-host> <db-port> <db-username> <db-password> <db-database>"
+    echo "Usage: $0 <namespace> <repo-name> <container-id> <db-host> <db-port> <db-username> <db-password> <db-database>"
     exit 1
 fi
 
 NAMESPACE=$1
-CONTAINER_ID=$2
-DB_HOST=$3
-DB_PORT=$4
-DB_USERNAME=$5
-DB_PASSWORD=$6
-DB_DATABASE=$7
+REPO_NAME=$2
+CONTAINER_ID=$3
+DB_HOST=$4
+DB_PORT=$5
+DB_USERNAME=$6
+DB_PASSWORD=$7
+DB_DATABASE=$8
 export NAMESPACE=$NAMESPACE
 export CONTAINER_ID=$CONTAINER_ID
+export REPO_NAME=$REPO_NAME
 export DB_HOST=$DB_HOST
 export DB_PORT=$DB_PORT
 export DB_USERNAME=$DB_USERNAME
